e68fd51532ce6d8ec02af7446adf8823c6e358f0
Anders Olson <sredna43@gmail.com>
2019-09-19 14:34:44 -0500

    Made all pages reference the same CSS class (App)

--- src/components/add.js
+++ src/components/add.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './nav';
 import '../css/App.css';
 
 
 function Add() {
   return (
-    <div className="Add">
+    <div className="App">
+      <Nav />
       <header className="App-header">
       <p>Add a new Record</p>
       </header>
--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/App.css';
-
+import Nav from './nav';
 
 function Search() {
   return (
-    <div className="Search">
+    <div className="App">
+      <Nav />
       <header className="App-header">
       <p>Search!</p>
       </header>
