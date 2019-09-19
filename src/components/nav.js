0ff78b47518e1ee6696d3ee7c470a7ff262d1767
Anders Olson <sredna43@gmail.com>
2019-09-18 19:07:59 -0500

    navbar file

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <div>Navbar Placeholder</div>
+            </div>
+        )
+    }
+}
+
+export default Nav;
